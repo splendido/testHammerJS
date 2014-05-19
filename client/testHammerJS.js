173cbcd9186ace0bb68ea3ed4431d0381a0d6cfe
Luca Mussi <luca.mussi@henesis.eu>
2014-05-19 14:17:34 +0200

    stop btn click propagation...

--- client/testHammerJS.js
+++ client/testHammerJS.js
@@ -1,15 +1,17 @@
 Template.hammerDiv.rendered = function() {
   $('#hammerDiv').hammer();
+  $('#btn').hammer();
 };
 
 Template.hammerDiv.events({
-  'doubletap #hammerDiv': function() {
-    alert("Double Click/Tap!");
+  'doubletap #hammerDiv': function(event) {
+    alert("Double Tap!");
   },
-  'hold #hammerDiv': function() {
+  'hold #hammerDiv': function(event) {
     alert("Hold!");
   },
-  'tap #btn': function(){
+  'tap #btn': function(event){
+    event.stopPropagation();
     alert("Yeah!");
   }
 });
