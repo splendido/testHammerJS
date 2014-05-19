fe558de6937ef8c8212eb9125b7fffa30a3534b6
Luca Mussi <luca.mussi@henesis.eu>
2014-05-19 19:04:54 +0200

    single hammer instance attached to body

--- client/testHammerJS.js
+++ client/testHammerJS.js
@@ -1,7 +1,14 @@
+
+UI.body.rendered = function(){
+  $('body').hammer();
+}
+
+/*
 Template.hammerDiv.rendered = function() {
   $('#hammerDiv').hammer();
   $('#btn').hammer();
 };
+*/
 
 Template.hammerDiv.events({
   'doubletap #hammerDiv': function(event) {
@@ -9,6 +16,15 @@ Template.hammerDiv.events({
   },
   'hold #hammerDiv': function(event) {
     alert("Hold!");
+  },
+  'pinchin #hammerDiv': function(event) {
+    alert("Pinch In!");
+  },
+  'pinchout #hammerDiv': function(event) {
+    alert("Pinch Out!");
+  },
+  'swiperight #hammerDiv': function(event) {
+    alert("Swipe Right!");
   },
   'tap #btn': function(event){
     event.stopPropagation();
